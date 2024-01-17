5efe7c4c2ac28920749adbca06c23ce0e479bd80
KING-VAJIRA <129604419+vajirabot1@users.noreply.github.com>
2024-01-18 02:25:34 +0530

    👑 ＫＩＮＧ - ＶＡＪＩＲＡ 👑

--- commands/moviedl.js
+++ commands/moviedl.js
@@ -92,3 +92,72 @@ cmd({
 
         }
     )
+
+//---------------------------------------------------------------------------
+
+cmd({
+    pattern: "movie2",
+    react: "⬇️",
+       desc: "Downloads video from yt.",
+       category: "downloader",
+       filename: __filename,
+       use: '<faded-Alan Walker>',
+   },
+   async(Void, citel, text) => {
+       let yts = require("secktor-pack");
+       let search = await yts(text);
+       let anu = search.videos[0];
+       let urlYt = anu.url
+       const getRandom = (ext) => {
+           return ${Math.floor(Math.random() * 10000)}${ext};
+       };
+           let infoYt = await ytdl.getInfo(urlYt);
+           if (infoYt.videoDetails.lengthSeconds >= videotime) return citel.reply(❌ Video file too big!);
+           let titleYt = infoYt.videoDetails.title;
+           let randomName = getRandom(".mp4");
+            citel.reply('_Download Your Video_')
+            citel.reply('_Upload Your Video_')
+
+           const stream = ytdl(urlYt, {
+                   filter: (info) => info.itag == 22 || info.itag == 18,
+               })
+               .pipe(fs.createWriteStream(./${randomName}));
+           await new Promise((resolve, reject) => {
+               stream.on("error", reject);
+               stream.on("finish", resolve);
+           });
+           let stats = fs.statSync(./${randomName});
+           let fileSizeInBytes = stats.size;
+           let fileSizeInMegabytes = fileSizeInBytes / (1024 * 1024);
+           if (fileSizeInMegabytes <= dlsize) {
+               Void.sendMessage(citel.chat, {
+                 image: {url: (search.all[0].thumbnail)},
+                 caption: `╔───────────────◆
+┊🧚 ${titleYt} 
+┊🚨 *Youtube Downloader* ✨
+┊ ┉━━━━◭☬◮━━━━━┉
+┊🌐 *Duration:* ${anu.timestamp}
+┊👀 *Viewers:* ${anu.views}
+┊⬆️ *Uploaded:* ${anu.ago}
+┊👽 *Author:* ${anu.author.name}
+╚────────────────◆
+⦿ *Url* : ${anu.url}`
+               }, { quoted: citel })
+               let buttonMessage = {
+                   document: fs.readFileSync(./${randomName}),
+                   mimetype: 'document/mp4',
+                   fileName: ${titleYt}.mp4,
+                   caption: ★[KING VAJIRA MD]★ 
+               }
+            Void.sendMessage(citel.chat, buttonMessage)
+            return fs.unlinkSync(./${randomName});
+           } else {
+               citel.reply(`❌ File size bigger than 100mb.`);
+           }
+           return fs.unlinkSync(`./${randomName}`);      
+
+
+   }
+)
+
+
